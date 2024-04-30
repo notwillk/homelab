88b71819c54e10facc6fe6099323ddfe97361b91
William Klancnik <william@klancnik.com>
2024-04-29 22:38:49 -0700

    Add run logging

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ANSIBLE_VAULT_PASSWORD_FILE=.vault_password.txt ansible-playbook -e @secrets.enc homelab.yml
+TIMESTAMP="$(date +%Y%m%d%H%M%S)"
+
+ANSIBLE_VAULT_PASSWORD_FILE=.vault_password.txt ANSIBLE_LOG_PATH=logs/ansible-$TIMESTAMP.log ansible-playbook -e @secrets.enc homelab.yml
