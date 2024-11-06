f71f1be36b8f5fcb39426d99033c639d45521628
notwillk <44582272+notwillk@users.noreply.github.com>
2024-11-06 03:44:22 +0000

    Playing with terraform

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "my_vm" {
+ ami                       = "ami-065deacbcaac64cf2" //Ubuntu AMI
+ instance_type             = "t2.micro"
+
+ tags = {
+   Name = "My EC2 instance",
+ }
+}
